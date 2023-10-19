827417ede21bd2150b3faaacb71d223bea71fefb
prashantnegi10 <prashantnegi478@gmail.com>
2023-10-19 17:56:35 +0530

    updated

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
 
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, './client/dist')));
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, './client/build/index.html'))
+    res.sendFile(path.join(__dirname, './client/dist/index.html'))
 });
 
 app.listen(PORT, () => {
